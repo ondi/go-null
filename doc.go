ad82be2987bcc78cd5a6a55b10c170a1036cc9ff
ondi <thy321@gmail.com>
2020-09-08 07:33:41 +0300

    doc.go update

--- doc.go
+++ doc.go
@@ -55,6 +55,31 @@ If the driver supports cursors, a returned Value may also implement the Rows int
 
 type Value interface{}
 
+type Rows
+Rows is an iterator over an executed query's results.
+
+type Rows interface {
+    // Columns returns the names of the columns. The number of
+    // columns of the result is inferred from the length of the
+    // slice. If a particular column name isn't known, an empty
+    // string should be returned for that entry.
+    Columns() []string
+
+    // Close closes the rows iterator.
+    Close() error
+
+    // Next is called to populate the next row of data into
+    // the provided slice. The provided slice will be the same
+    // size as the Columns() are wide.
+    //
+    // Next should return io.EOF when there are no more rows.
+    //
+    // The dest should not be written to outside of Next. Care
+    // should be taken when closing Rows not to modify
+    // a buffer held in dest.
+    Next(dest []Value) error
+}
+
 */
 
 package null
