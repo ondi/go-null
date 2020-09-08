980827bc676c12396555da2e6ef249172af0c4bd
ondi <thy321@gmail.com>
2020-09-08 07:35:59 +0300

    doc.go update

--- doc.go
+++ doc.go
@@ -43,7 +43,9 @@ type Valuer interface {
 }
 
 type Value
-Value is a value that drivers must be able to handle. It is either nil, a type handled by a database driver's NamedValueChecker interface, or an instance of one of these types:
+Value is a value that drivers must be able to handle. It is either nil,
+a type handled by a database driver's NamedValueChecker interface,
+or an instance of one of these types:
 
 int64
 float64
@@ -51,7 +53,10 @@ bool
 []byte
 string
 time.Time
-If the driver supports cursors, a returned Value may also implement the Rows interface in this package. This is used, for example, when a user selects a cursor such as "select cursor(select * from my_table) from dual". If the Rows from the select is closed, the cursor Rows will also be closed.
+If the driver supports cursors, a returned Value may also implement
+the Rows interface in this package. This is used, for example,
+when a user selects a cursor such as "select cursor(select * from my_table) from dual".
+If the Rows from the select is closed, the cursor Rows will also be closed.
 
 type Value interface{}
 
